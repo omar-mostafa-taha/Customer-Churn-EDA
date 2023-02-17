e03b75b2bb364abfcfa1aa9ece463991f94868bb
omar-mostafa-taha <66518667+omar-mostafa-taha@users.noreply.github.com>
2023-02-17 22:23:39 +0200

    Update Home.py

--- Home.py
+++ Home.py
@@ -4,7 +4,7 @@ st.title('Telco Customer Churn EDA')
 st.image('churn.png', caption='Let the Butter Churn, Not Your Customers!', use_column_width=True)
 
 dataset = '- View The Dataset on Kaggle  [Dataset](https://www.kaggle.com/datasets/blastchar/telco-customer-churn)'
-kaggle = '- View my work on Kaggle  [Kaggle notebook](https://www.kaggle.com/code/omarmostafataha/customer-churn-eda)'
+kaggle = '- View my work on Kaggle  [Kaggle notebook](https://www.kaggle.com/omarmostafataha/customer-churn-eda-seaborn-and-plotly-streamlit)'
 linked = '- View my LinkedIn Profile  [LinkedIn Profile](https://www.linkedin.com/in/omar-mostafa-96a840210)'
 github = '- Check the GitHub Project Repository  [Github Repo](https://github.com/omar-mostafa-taha/Customer_Churn.git)'
 
